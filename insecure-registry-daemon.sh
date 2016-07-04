30ad45cc935ce9cc0a5064a17222744d57d56f46
Brayden Li <brayden.li@ef.com>
2016-07-04 13:31:22 +0800

    better support for non-boot2docker environments

--- insecure-registry-daemon.sh
+++ insecure-registry-daemon.sh
@@ -3,11 +3,22 @@
 # docker daemon start script
 [ $(id -u) = 0 ] || { echo 'must be root' ; exit 1; }
 
+__is_boot2docker () {
+	uname -a | grep "boot2docker" && return 0;
+	return 1;
+}
+
 # make sure daemon is running
 if [ ! -f /var/run/docker.pid ]; then
   echo 'docker daemon not running, try to start it...'
+
   # start docker daemon
-  /etc/init.d/docker start
+  if [ __is_boot2docker ]; then
+	/etc/init.d/docker start
+  else
+    service docker start
+  fi
+
   if [ ! -f /var/run/docker.pid ]; then
     echo 'failed to start docker daemon. exit.'
     exit 1
@@ -15,25 +26,26 @@ if [ ! -f /var/run/docker.pid ]; then
 fi
   
 pid=$(cat /var/run/docker.pid)
-#echo $pid
+# echo $pid
 
 cmd=$(cat /proc/$pid/cmdline | tr '\000' ' ')
-#echo $cmd
+# echo $cmd
+
+echo $cmd | grep "\-\-insecure\-registry 10.128.43.55:5000" 1>/dev/null && { echo "insecure registry 10.128.43.55:5000 already enabled. skip and exit."; exit 0; }
 
 cmd="$cmd -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --insecure-registry 10.128.43.55:5000"
-echo $cmd
 
-cat > docker.sh << EOF
-#!/bin/sh
+echo "stop docker daemon first"
+if [ __is_boot2docker ]; then
+  /etc/init.d/docker stop
+else
+  service docker stop
+fi
 
-# check sudo 
-[ $(id -u) = 0 ] || { echo 'must be root' ; exit 1; }
-
-# stop daemon first
-/etc/init.d/docker stop
-
-# start daemon with new args
-$cmd >> /var/lib/boot2docker/docker.log 2>&1 &
-EOF
-
-[ -f docker.sh ] && chmod +x docker.sh && ./docker.sh && rm docker.sh
+echo "start daemon with new args"
+echo "new cmd: $cmd"
+if [ __is_boot2docker ]; then
+  $cmd >> /var/lib/boot2docker/docker.log 2>&1 &
+else
+  $cmd &
+fi
